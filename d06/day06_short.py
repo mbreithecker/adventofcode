b18ce437693c87ed803bdf55704f9ce19dbd9d42
Maximilian Breithecker <m@breithecker.de>
2021-12-06 16:52:38 +0100

    removed unnecessary code

--- d06/day06_short.py
+++ d06/day06_short.py
@@ -4,9 +4,8 @@ for fish_timer in [int(p) for p in open('input.txt', 'r').read().split(",")]:
     fishShift[fish_timer] += 1
 
 for day in range(0, 256):
-    newShift = [fishShift[i+1] for i in range(8)] + [fishShift[0]] # shift array
-    newShift[6] += fishShift[0]  # reset zero fish-timer to 6 days
-    fishShift = newShift
+    fishShift = [fishShift[i+1] for i in range(8)] + [fishShift[0]] # shift array
+    fishShift[6] += fishShift[8]  # reset zero fish-timer to 6 days, zeros are now in the 8th place after shift
     if day == 79:
         print("Part 1:", sum(fishShift))
 
